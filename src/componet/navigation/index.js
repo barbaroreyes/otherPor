b0ab2b798975346e03db9c6ad57a9cd9fc20ec99
barbaro reyes <barbaroreyes@barbaros-iMac.attlocal.net>
2021-01-12 19:29:24 -0600

    nav

---
+++ src/componet/navigation/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './nav.css';
+
+
+const Nav = () => {
+  return (
+      <ul className='navi'>
+      <li><a href="#home">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#portfolio">Portfolio</a></li>
+      <li><a href="#contact">Contact</a></li>
+      </ul>
+  )
+}
+export default Nav

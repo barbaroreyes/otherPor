bb519dabb1f452112c7eb01759f3b1f18cd0fbde
barbaro reyes <barbaroreyes@barbaros-iMac.local>
2021-01-12 22:19:19 -0600

    upt3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-import Nav from './componet/navigation/index'
+import Nav from './componet/navigation/index';
+import Main from './componet/main/main'
+
+
 
 function App() {
   return (
     <div className="App">
      <Nav/>
+     <Main/>
     </div>
   );
 }
--- src/componet/navigation/index.js
+++ src/componet/navigation/index.js
@@ -4,12 +4,17 @@ import './nav.css';
 
 const Nav = () => {
   return (
-      <ul className='navi'>
-      <li><a href="#home">Home</a></li>
-      <li><a href="#about">About</a></li>
-      <li><a href="#portfolio">Portfolio</a></li>
-      <li><a href="#contact">Contact</a></li>
-      </ul>
+    <header className='navi'>
+    <nav>
+        <ul id="navi">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </nav>
+    <div class="name">Barbaro Fernando Thinkful</div>
+</header>
   )
 }
 export default Nav
